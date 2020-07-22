1c41b483d208b9347d4c4af2304e02def0bf9db6
CharanMareedu-REISys <charan.mareedu@reisystems.com>
2020-07-22 13:59:33 -0400

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,18 +20,19 @@ export interface Tile {
 })
 export class AppComponent {
 
-  tiles: Tile[] = [
-    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 3, rows: 1, color: 'lightgreen'}
-  ];
+  opened: boolean = true;
+
   options = {
-    pscValue : ['Onefasfadsfafsdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdffadsfasdfasdfa', 'Two', 'Three']
+    pscValue : ['One', 'Two', 'Three'],
+    naicsValue : ['Naics1','Niacs2']
   };
+
   pscFilteredOptions: Observable<string[]>;
 
   profileForm = this.fb.group({
     fromDate: [],
     pscValue: [''],
+    naicsValue:[''],
     textValue: ['']
   });
 
